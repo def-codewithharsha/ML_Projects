73988304099ccd64a8512f7516e8bc7ba785e1cf
unknown <codewithharsha@gmaile.com>
2026-01-08 16:13:16 +0530

    add RandomForest class with methods fit,bootstrap,predict

--- randomforest.py
+++ randomforest.py
@@ -89,3 +89,41 @@ class DecisionTree:
 
     def predict(self, X):
         return np.array([self._predict_sample(x, self.root) for x in X])
+class RandomForest:
+    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2, max_features=None):
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_features = max_features
+        self.trees = []
+    def _bootstrap_sample(self, X, y):
+        n_samples = X.shape[0]
+        indices = np.random.choice(n_samples, n_samples, replace=True)
+        return X[indices], y[indices]
+    def fit(self, X, y):
+        n_features = X.shape[1]
+
+        if self.max_features is None:
+            self.max_features = int(np.sqrt(n_features))
+
+        self.trees = []
+
+        for _ in range(self.n_trees):
+            tree = DecisionTree(
+                max_depth=self.max_depth,
+                min_samples_split=self.min_samples_split,
+                max_features=self.max_features
+            )
+
+            X_sample, y_sample = self._bootstrap_sample(X, y)
+            tree.fit(X_sample, y_sample)
+            self.trees.append(tree)
+    def predict(self, X):
+        tree_preds = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(tree_preds, 0, 1)
+
+        predictions = []
+        for sample_preds in tree_preds:
+            predictions.append(Counter(sample_preds).most_common(1)[0][0])
+
+        return np.array(predictions)
