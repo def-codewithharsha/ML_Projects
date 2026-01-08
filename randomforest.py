caf2e4298247e590eac4e7f5079b8b3634720639
unknown <codewithharsha@gmaile.com>
2026-01-08 16:06:41 +0530

    add decision tree class

--- randomforest.py
+++ randomforest.py
@@ -1,3 +1,7 @@
+import numpy as np
+from collections import Counter
+
+
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
         self.feature = feature
@@ -5,3 +9,10 @@ class Node:
         self.left = left
         self.right = right
         self.value = value
+
+class DecisionTree:
+    def __init__(self, max_depth=10, min_samples_split=2, max_features=None):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_features = max_features
+        self.root = None
